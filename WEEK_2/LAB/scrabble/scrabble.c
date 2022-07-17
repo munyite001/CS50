3ab4fcc2332164099e86fb196261a8e3663c2a09
munyite001 <emunyite@gmail.com>
2022-07-17 18:50:03 +0300

    Finished the lab 'Scrabble'

---
+++ WEEK_2/LAB/scrabble/scrabble.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    //  Alphabets array
+    char Alphabets[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int wordlength = strlen(word);
+    int Arrlength = strlen(Alphabets);
+    int score = 0;
+
+//  For every letter in the word, we'll loop through the alphabet array, checking for the index position of that letter
+//  Then we'll go to the points array, and add the number at that index position, to the score variable
+    for (int i = 0; i < wordlength; i++)
+    {
+        for (int j = 0; j < Arrlength; j++)
+        {
+            if (toupper(word[i]) == Alphabets[j])
+            {
+                score += POINTS[j];
+            }
+        }
+    }
+
+    return score;
+}
---
+++ WEEK_2/Practice/scores.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char message[3] = "HI!";
+    printf("%i %i %i %i\n",message[0],message[1],message[2],message[4]);
+
+}
